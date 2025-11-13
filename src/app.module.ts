d0ed0a3c4adbb074f8cb0f7937f1a40d2fc561af
scxrrow <noahfelten@icloud.com>
2025-11-14 00:03:49 +0100

    ajout des premieres fonctionnalités de l'authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { GamesController } from './games/games.controller';
 import { GamesService } from './games/games.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController, GamesController],
   providers: [AppService, GamesService],
 })
